646f1a6fe096eb5e973d20f9d7ec3dd074c6e7e7
Mercedes Scattini <56845948+MechiScattini@users.noreply.github.com>
2024-10-24 19:47:01 -0300

    Update SpringDataConfig.java

--- src/main/java/com/dssd/BackendApi/config/SpringDataConfig.java
+++ src/main/java/com/dssd/BackendApi/config/SpringDataConfig.java
@@ -52,7 +52,7 @@ public class SpringDataConfig {
     private final Properties hibernateProperties() {
         Properties hibernateProperties = new Properties();
 
-        hibernateProperties.setProperty("hibernate.hbm2ddl.auto", "create-drop");
+        hibernateProperties.setProperty("hibernate.hbm2ddl.auto", "update");
         hibernateProperties.setProperty(
                 "hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
         hibernateProperties.setProperty("hibernate.show_sql", "true");
